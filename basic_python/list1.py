1849de64ac983f1561d9054d8cedc3c074cb4ee4
Yhudi Cornelius <abrahes12@yahoo.com>
2021-10-14 18:57:45 +0700

    add basic learn python

---
+++ basic_python/list1.py
@@ -0,0 +1,5 @@
+s = input('Enter your sentence: ')
+print('Your Sentence is: '+s)
+w1 = input('Enter the word to replace: ')
+w2 = input('Enter the word to replace it with: ')
+print(s.replace(w1,w2))
